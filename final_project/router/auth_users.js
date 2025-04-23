8c17cb9e5798292c6bb607fe5edf58e45c84fc9c
chetanjangir0 <chetanrjj2002@gmail.com>
2025-04-23 19:09:09 +0530

    added /customer/login function and changed a few things in general.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,35 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+  const filtered = users.filter((usr) => usr.username === username);
+  return filtered.length > 0;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+  const filtered = users.filter((usr) => usr.username === username && usr.password === password);
+  return filtered.length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!(username && password)) {
+   return res.status(404).json({message:"Error logging in"});
+  }
+
+  if (!authenticatedUser(username, password)) {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
+
+  let accessToken = jwt.sign({
+    data:password
+  }, "access", {expiresIn: 60*60});
+  
+  req.session.authorization = {
+    accessToken, username
+  }
+  return res.status(200).json({message:"User successfully logged in"}); 
 });
 
 // Add a book review
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,34 +4,29 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-function doesExist(username) {
-  const filtered = users.filter((usr) => usr.username === username);
-  return filtered.length > 0;
-}
-
 public_users.post("/register", (req,res) => {
   username = req.body.username;
   password = req.body.password;
-  if (! (username && password)) {
-    res.status(404).json({message: "Unable to register user"});
+  if (!(username && password)) {
+    return res.status(404).json({message: "Unable to register user"});
   }
-  if (doesExist(username)) {
-    res.status(404).json({message: "user already exists!"});
+  if (isValid(username)) {
+    return res.status(404).json({message: "user already exists!"});
   }
   users.push({username, password});
-  res.status(200).json({message: "user successful registered"});
+  return res.status(200).json({message: "user successful registered"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.json(books);
+  return res.json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   if (isbn in books) {
-    res.json(books[isbn])
+    return res.json(books[isbn])
   }
   return res.status(404).json({message: "book not found"});
  });
@@ -41,7 +36,7 @@ public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
   for (let isbn in books) {
     if (author === books[isbn].author) {
-      res.json(books[isbn]);
+      return res.json(books[isbn]);
     }
   }
   return res.status(404).json({message: "author not found"});
@@ -52,7 +47,7 @@ public_users.get('/title/:title',function (req, res) {
   const title = req.params.title;
   for (let isbn in books) {
     if (title === books[isbn].title) {
-      res.json(books[isbn]);
+      return res.json(books[isbn]);
     }
   }
   return res.status(404).json({message: "title not found"});
@@ -62,7 +57,7 @@ public_users.get('/title/:title',function (req, res) {
 public_users.get('/review/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   if (isbn in books) {
-    res.json(books[isbn].reviews);
+    return res.json(books[isbn].reviews);
   }
   return res.status(404).json({message: "book not found"});
 });
